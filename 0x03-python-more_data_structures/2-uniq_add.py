972677144a374d1d5cd027497e7a02cad31cb88c
Malik <85846069+Wolverine5696@users.noreply.github.com>
2022-05-09 23:15:01 -0500

    added answer

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -4,4 +4,9 @@ def uniq_add(my_list=[]):
     lista = list(lista)
     add = sum(lista)
     return add
-    
+    #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    lista = set(my_list)
+    lista = list(lista)
+    add = sum(lista)
+    return add
