ab069e66f7ff27bcf5011673d382177c667b76c0
Malik <85846069+Wolverine5696@users.noreply.github.com>
2022-05-20 16:56:34 -0500

    adding answer

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-
-
-def argvprint(argv):
-    j = len(argv)
-
-    if j == 1:
-        print("0 arguments.")
-    elif j == 2:
-        print("1 argument:\n1: {}".format(str(sys.argv[1])))
-    else:
-        print("{} arguments:".format(j - 1))
-        for i in range(len(sys.argv)):
-            if i != 0:
-                print("{}: {}".format(i, str(sys.argv[i])))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
-    argvprint(sys.argv)
+
+if len(sys.argv) == 1:
+    print("{} arguments.".format(len(sys.argv) - 1))
+elif len(sys.argv) == 2:
+    print("{} argument:".format(len(sys.argv) - 1))
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+
+for a in range(1, len(sys.argv)):
+    print("{}: {}".format(a, sys.argv[a]))
