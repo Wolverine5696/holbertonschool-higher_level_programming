8acf151573d80a231d929ce86c5ef494ed50c4a0
Malik <85846069+Wolverine5696@users.noreply.github.com>
2022-05-20 16:58:25 -0500

    adding answer

---
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        prod = a / b
+    except:
+        prod = None
+    finally:
+        print("Inside result: {}".format(prod))
+        return prod
