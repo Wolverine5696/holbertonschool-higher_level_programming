ffcc5a935a8e14eff63c78c44aa377bdd3390d82
Malik <85846069+Wolverine5696@users.noreply.github.com>
2022-05-20 21:25:25 -0500

    removing whitespace

--- 0x00-python-hello_world/2-positive_or_negative.py
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 import random
 number = random.randint(-10, 10)
 
@@ -9,5 +8,4 @@ if number > 0:
 elif number < 0:
     print("{} is negative".format(number))
 else:
-    print("0 is zero")
-    
+    print("0 is zero")   
