4f94e1356de8d42eeea7bed8d4bff49caa9c687b
Malik <85846069+Wolverine5696@users.noreply.github.com>
2022-05-20 20:32:41 -0500

    fixed answer or trying to fix answer

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
 """
-Module for divide Function
+matrix_divided:
+    Divides all variables of a matrix
+    by div
+    Returns new matrix of ints rounded to
+    two dec places
 """
+
+list_error = "matrix must be a matrix (list of lists) of integers/floats"
 
 
 def matrix_divided(matrix, div):
     """
-    Function That Divides All Elementes of a Matrix
+    Checks if matrix is a list
+    Checks each row is the same
+    Checks div is not 0
     """
-    error = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if isinstance(matrix, list) or len(matrix) < 1:
-        raise TypeError(error)
+    if type(matrix) is not list:
+        raise TypeError(list_error)
+    size = len(matrix[0])
+    for row in matrix:
+        if size != len(row):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in row:
+            if type(i) not in (int, float):
+                raise TypeError(list_error)
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if not isinstance(matrix[0], list):
-            raise TypeError(error)
-
-    len_matrix = len(matrix[0])
-    new_list = []
-
-    for count, i in enumerate(matrix):
-        if isinstance(i, list) or len(i) < 1:
-            raise TypeError(error)
-
-        if len(i) != len_matrix:
-            raise TypeError("Each row of the matrix must have the same size")
-
-        new_list.append([])
-        for j in i:
-            if isinstance(j, (int, float)):
-                raise TypeError(error)
-            new_list[count].append(round(j / div, 2))
-    return new_list
+    return [[round(i / div, 2) for i in row] for row in matrix]
