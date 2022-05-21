233e02bbf9f45c95bca6b3a665c94ad530e9c697
Malik <85846069+Wolverine5696@users.noreply.github.com>
2022-05-20 20:35:23 -0500

    fixed code

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,31 +1,20 @@
-
-
 #!/usr/bin/python3
 """
 Prints "Prints a square"
 """
 
 
-def text_indentation(text):
+def print_square(size):
     """
     Checks to make sure size is an int > 0, then prints a square
     """
-    if type(text) is not str:
-        raise TypeError("text must be a string")
-
-    line_breaks = ['.', '?', ':']
-
-    flag = 0
-    for i in text:
-        if flag == 0:
-            if i == ' ':
-                continue
-            else:
-                flag = 1
-        if flag == 1:
-            if i in line_breaks:
-                print(i)
-                print()
-                flag = 0
-            else:
-                print(i, end="")
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
