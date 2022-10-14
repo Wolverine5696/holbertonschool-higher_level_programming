4b0b0cb062d58e342260867e5e43679f573ef9a2
Malik <85846069+Wolverine5696@users.noreply.github.com>
2022-10-13 23:48:57 -0500

    adding files to git

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -8,6 +8,6 @@ the specified class ; otherwise returns false.
 
 def is_kind_of_class(obj, a_class):
     """ This function returns true if the object is a instance of, if the
-        object is an instance of a class that  it
+        object is an instance of a class that it
         inherited from"""
     return isinstance(obj, a_class)
