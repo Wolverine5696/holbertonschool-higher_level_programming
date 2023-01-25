2f6e60a17f4acf212492c54586b8698ca7840449
Malik <85846069+Wolverine5696@users.noreply.github.com>
2023-01-24 23:09:08 -0600

    fixing question 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
 """
-This is the function about  5-text_indentation.py
+Prints "Prints a square"
 """
 
 
 def text_indentation(text):
-    '''Function that writes a text with 2 new lines
-    after each of the 2 characters: .,? and :
-    '''
-    if isinstance(text, str):
+    """
+    makes sure size is an int > 0, prints a square
+    """
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    i = 0
-    len_text = len(text)
-    while i < len_text:
-        if text[i] in ('.', '?', ':'):
-            print("{}\n".format(text[i]))
-            while i + 1 < len_text and text[i + 1] == " ":
-                i += 1
-        else:
-            print(text[i], end='')
-        i += 1
+    line_breaks = ['.', '?', ':']
+
+    flag = 0
+    for i in text:
+        if flag == 0:
+            if i == ' ':
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if i in line_breaks:
+                print(i)
+                print()
+                flag = 0
+            else:
+                print(i, end="")
