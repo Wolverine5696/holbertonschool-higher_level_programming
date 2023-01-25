8377518a4165a8868c6b787eb4b5dc9f30d9dbe6
Malik <85846069+Wolverine5696@users.noreply.github.com>
2023-01-24 23:24:26 -0600

    changing answers

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """
-This is the module 3-say_my_name.py
+Prints "My name is First_name, Last_name"
 """
+
 
 def say_my_name(first_name, last_name=""):
     """
-    Write a function that prints My name is <first name> <last name>
+    Checks to make sure both passed variables is a string
     """
-    if type(first_name) is not str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
