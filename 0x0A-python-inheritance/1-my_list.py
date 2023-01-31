ad075ef6d9f90114fd44f9c6896813cfe01be86c
Malik <85846069+Wolverine5696@users.noreply.github.com>
2023-01-31 14:05:12 -0600

    corrected answer

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,11 @@ my list class that inherits from a list
 
 class mylist(list):
     """Public instance method that prints the list, but sorted (ascending sort)"""
+    def __init__(self):
+        """initializes the object using the super()"""
+        super().__init__()
+        
     def print_sorted(self):
+        """prints itself sorted"""
         print(sorted(self))
      
