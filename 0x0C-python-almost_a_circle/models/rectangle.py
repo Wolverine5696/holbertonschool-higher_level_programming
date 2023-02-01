30d93552164c07d1af154b047d799ceefdf009ec
Malik <85846069+Wolverine5696@users.noreply.github.com>
2023-01-31 20:28:54 -0600

    add files and changes to push and commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        init method
+        initializes method
         """
         self.width = self.hw_validator("width", width)
         self.height = self.hw_validator("height", height)
