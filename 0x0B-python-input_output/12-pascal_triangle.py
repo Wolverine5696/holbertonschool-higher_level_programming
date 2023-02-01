ce944069c091c391f3d1ce19fdcfa92709e07236
Malik <85846069+Wolverine5696@users.noreply.github.com>
2023-01-31 19:17:27 -0600

    fixed errors

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""
+Pascal's Triangle not using Magic 11s
+"""
+
+
 def pascal_triangle(n):
-    
+    """
+    Returns: list of pascal's Triangle
+    """
+    triangle = []
+    for i in range(n):
+        triangle.append([])
+        for j in range(i + 1):
+            if j == i or j == 0:
+                triangle[i].append(1)
+            else:
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+    return triangle
